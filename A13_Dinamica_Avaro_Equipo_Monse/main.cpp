89548b6d47eafb55aa049cca5aea5a7d41029c43
MonseMoreno <a01276775@tec.mx>
2022-09-10 19:42:34 -0500

    comentarios

--- A13_Dinamica_Avaro_Equipo_Monse/main.cpp
+++ A13_Dinamica_Avaro_Equipo_Monse/main.cpp
@@ -1,115 +1,126 @@
-//Codigo trabajado por Monserrat Karime Moreno Casas 
-//Actividad 1.3 Implementación de la técnica de programación "Programación dinámica" y "algoritmos avaros"
+// Codigo trabajado por Monserrat Karime Moreno Casas
+// Actividad 1.3 Implementación de la técnica de programación "Programación dinámica" y "algoritmos avaros"
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
+// Complejidad del algoritmo: O(n*m)
 
-//Complejidad del algoritmo: O(n*m)
-
-void AlgoritmoDinamico(int monedas[], int monto, int Array_Size) {
+void AlgoritmoDinamico(int monedas[], int monto, int Array_Size) // Monedas, cantidadde monto y una cadena con las medidas
+{
 
     int max = monto + 1;
     int dp[monto + 1];
 
-    for (int i = 0; i < monto + 1; i++) {
+//ciclo for para el monto maximo
+    for (int i = 0; i < monto + 1; i++)
+    {
         dp[i] = max;
     }
 
     dp[0] = 0;
 
-    for (int i = 1; i <= monto; i++) {
+    for (int i = 1; i <= monto; i++)
+    {
 
-        for (int j = 0; j < Array_Size; j++) {
+        for (int j = 0; j < Array_Size; j++)
+        {
 
-            if (monedas[j] <= i) {
+            if (monedas[j] <= i)
+            {
                 dp[i] = min(dp[i], dp[i - monedas[j]] + 1);
             }
-
         }
-
     }
 
+
+//Verificación de la cantidad minima de monedas
     int num_monedas = (dp[monto] > monto) ? -1 : dp[monto];
 
-    if (num_monedas != -1) {
+    if (num_monedas != -1)
+    {
         cout << "\nNúmero mínimo de monedas de cambio del producto: " << num_monedas << endl;
     }
-    else {
+    else
+    {
         cout << "\nCambio insuficiente!! :c" << endl;
     }
-
 }
 
+// Complejidad del algoritmo: O(n*m)
 
-//Complejidad del algoritmo: O(n*m)
-
-
-
-void AlgoritmoAvaro(int monedas[], int monto, int Array_Size) {
+void AlgoritmoAvaro(int monedas[], int monto, int Array_Size)
+{
 
     sort(monedas, monedas + Array_Size); // Ordena monedas
     vector<int> ans;
 
-    for (int i = Array_Size - 1; i >= 0; i--) {
+// For con la medida de la cadena
+    for (int i = Array_Size - 1; i >= 0; i--)
+    {
 
-        while (monto >= monedas[i]) {
+        while (monto >= monedas[i])
+        {
 
             monto -= monedas[i];
-            ans.push_back(monedas[i]);
-
+            ans.push_back(monedas[i]); // Push de las monedas a answer
         }
-
     }
 
-    int num_monedas = ans.size();
+    int num_monedas = ans.size(); // Igualar medida monedas
 
-    if (num_monedas != 0) {
+    if (num_monedas != 0)
+    {
 
-        if (monto == 0) {
+        if (monto == 0)
+        {
 
             cout << "\nNúmero mínimo de monedas de cambio del producto: " << num_monedas << endl;
-            cout << "\n- Monedas que se dan de cambio -\n" << endl;
+            cout << "\n- Monedas que se dan de cambio -\n"
+                << endl;
 
-            for (int i = 0; i < ans.size(); i++) {
+            for (int i = 0; i < ans.size(); i++)
+            {
                 cout << "Moneda " << i + 1 << ": $" << ans[i] << endl;
             }
-
         }
-        else {
+        else
+        {
             cout << "\nEn este algoritmo no existe solución" << endl;
         }
-
     }
-    else {
+    else
+    {
         cout << "\nCambio insuficiente!!!" << endl;
     }
-
 }
 
-int main() {
+int main()
+{
     cout << "************************************************************" << endl;
     cout << "*********************  Actividad 1.3  **********************" << endl;
     cout << "************************************************************" << endl;
-  
+
     cout << "\nEscribe el número de monedas a ingresar: ";
 
     int n;
     cin >> n;
-    int monedas[n];
+    int monedas[n]; // Array para la cantidad de monedas
 
-    cout << "\n***** Escriba el valor de las monedas *****\n" << endl;
+    cout << "\n***** Escriba el valor de las monedas *****\n"
+        << endl;
 
-    for (int i = 0; i < n; i++) {
+// Ciclo para que el usuario pueda insertar las monedas deseadas
+    for (int i = 0; i < n; i++)
+    {
 
         cout << "Moneda " << i + 1 << ": $";
 
         int temp;
         cin >> temp;
         monedas[i] = temp;
-
     }
 
     int p, q, algorithm;
@@ -119,33 +130,37 @@ int main() {
     cout << "Escribe la cantidad con la que se pagará el producto: $";
     cin >> q; // Cantidad pagada
 
-  
     cout << "\n************** Menú de Algoritmos ****************" << endl;
     cout << "1. Algoritmo de programación avara" << endl;
     cout << "2. Algoritmo de programación dinámica" << endl;
     cout << "**************************************************" << endl;
 
-    do {
+
+// MENU PARA ELEGIR EL TIPO DE ALGORITMO
+    do
+    {
 
         cout << "\nElige el algoritmo de la solución: ";
         cin >> algorithm;
 
-        if (algorithm == 1) {
+
+        // ALGORITMO AVARO
+        if (algorithm == 1)
+        {
 
             cout << "\nCambio: $" << q - p << endl;
             AlgoritmoAvaro(monedas, q - p, n);
-
         }
-        else
-            if (algorithm == 2) {
 
-                cout << "\nCambio: $" << q - p << endl;
-                AlgoritmoDinamico(monedas, q - p, n);
+        // ALGORITMO DINÁMICO
+        else if (algorithm == 2)
+        {
 
-            }
+            cout << "\nCambio: $" << q - p << endl;
+            AlgoritmoDinamico(monedas, q - p, n);
+        }
 
     } while (algorithm != 1 && algorithm != 2);
 
     return 0;
-
 }
